189e273d580af29009403160a001cf6d97e15622
diddibjoss <diddibjoss@gmail.com>
2016-04-26 21:58:20 +0000

    Initial project

---
+++ WebApplicationMooshak/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplicationMooshak.Startup))]
+namespace WebApplicationMooshak
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
